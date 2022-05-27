51d3dbffa4d62f4af0b800220b423b7b72cf60ec
Atri Hegde <iamatrihegde@gmail.com>
2022-05-27 23:51:29 +0100

    arch bspwm rice

---
+++ bash/.bashrc
@@ -0,0 +1,36 @@
+#
+# ~/.bashrc
+#
+
+# If not running interactively, don't do anything
+[[ $- != *i* ]] && return
+
+alias ls='exa --icons'
+PS1='[\u@\h \W]\$ '
+
+PATH=$PATH:/home/mizuuu/.local/bin
+. "$HOME/.cargo/env"
+
+
+
+alias ll='exa --icons -l'
+alias la='exa --icons -la'
+
+alias update='paru'
+alias yay='paru'
+# alias cat='bat'
+alias install='sudo pacman -S'
+alias pi='ssh mizuuu@192.168.1.3'
+
+alias gs='git status'
+alias ga='git add -A .'
+alias gc='git commit -m'
+alias gp='git push'
+alias push='git push'
+
+alias bsh='vim ~/.bashrc'
+alias dot='cd ~/.dotfiles'
+alias sx='vim ~/.config/bspwm/sxhkd/sxhkdrc'
+alias bsp='vim ~/.config/bspwm/bspwmrc'
+alias poly='vim ~/.config/polybor/config.ini'
+
---
+++ rofi/.config/rofi/applets/applets/screenshot.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+style="$($HOME/.config/rofi/applets/applets/style.sh)"
+
+dir="$HOME/.config/rofi/applets/applets/configs/$style"
+rofi_command="rofi -theme $dir/screenshot.rasi"
+
+# Error msg
+msg() {
+	rofi -theme "$HOME/.config/rofi/applets/styles/message.rasi" -e "Please install 'scrot' first."
+}
+
+# Options
+screen=""
+area=""
+window=""
+
+# Variable passed to rofi
+options="$screen\n$area\n$window"
+
+chosen="$(echo -e "$options" | $rofi_command -p 'scrot' -dmenu -selected-row 1)"
+case $chosen in
+    $screen)
+		if [[ -f /usr/bin/scrot ]]; then
+			sleep 1; scrot '/tmp/%F_%T_$wx$h.png' -e 'xclip -selection clipboard -target image/png -i $f'
+		else
+			msg
+		fi
+        ;;
+    $area)
+		if [[ -f /usr/bin/scrot ]]; then
+			scrot -s '/tmp/%F_%T_$wx$h.png' -e 'xclip -selection clipboard -target image/png -i $f'
+		else
+			msg
+		fi
+        ;;
+    $window)
+		if [[ -f /usr/bin/scrot ]]; then
+			sleep 1; scrot -u '/tmp/%F_%T_$wx$h.png' -e 'xclip -selection clipboard -target image/png -i $f'
+		else
+			msg
+		fi
+        ;;
+esac
+
