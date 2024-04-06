21de5651ed37b0d75d5c421f87ca65d54d1deb13
Atri Hegde <atri@hegdeatri.com>
2024-04-06 17:55:50 +0100

    updates

--- idk/hypr/scripts/toggle_animations.sh
+++ idk/hypr/scripts/toggle_animations.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==2{print $2}')
+#!/usr/bin/env sh
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | awk 'NR==1{print $2}')
 if [ "$HYPRGAMEMODE" = 1 ] ; then
     hyprctl --batch "\
         keyword animations:enabled 0;\
